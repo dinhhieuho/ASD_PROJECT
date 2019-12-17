6cc5993c1db891a049a7341742989182db5ed51f
Dinh Hieu Ho <dhho@mum.edu>
2019-12-16 20:36:30 -0600

    added mainviewFW

--- src/banking/ReportController.java
+++ src/banking/ReportController.java
@@ -1,5 +1,13 @@
 package banking;
 
+import java.util.Map;
+
 public class ReportController extends BankController{
+
+	@Override
+	public void actionHandler(Map<String, String> arg) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/framework/Finco.java
+++ src/framework/Finco.java
@@ -2,16 +2,18 @@ package framework;
 
 import framework.ui.FincoDefaultView;
 import framework.ui.FincoView;
+import framework.ui.MainViewFW;
 
 public class Finco {
 
 	FincoView view = new FincoDefaultView();
+	FincoView mainView = new MainViewFW();
 	
 	public static void main(String[] args) {
 		(new Finco()).run();
 	}
 	public void run() {
-		view.run();
+		mainView.run();
 	}
 	public void setView(FincoView view) {
 		this.view = view;
