8903b61db27bb88f31bd667774ba7e8287058416
610530 <610530@cs105-02-01.cs.mum.edu>
2019-12-17 12:30:18 -0600

    rebase

--- src/creditcard/ReportController.java
+++ src/creditcard/ReportController.java
@@ -1,6 +1,14 @@
 package creditcard;
 
+import java.util.Map;
+
 public class ReportController extends CcardController{
+
+	@Override
+	public void actionHandler(Map<String, String> arg) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	@Override
 	public void actionHandler() {
