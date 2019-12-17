c1e217ed8c52f17685d800df6064ade178c2663f
610530 <610530@cs105-02-01.cs.mum.edu>
2019-12-17 17:38:04 -0600

    banking again

--- src/banking/ReportController.java
+++ src/banking/ReportController.java
@@ -4,10 +4,5 @@ import java.util.Map;
 
 public class ReportController extends BankController{
 
-	@Override
-	public void actionHandler(Map<String, String> arg) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
